c78c056bc800d492112e92ac09a5009abac0221e
김서연 <86418674+flowersayo@users.noreply.github.com>
2023-06-13 14:48:29 +0900

    feat: associate interface progress with stageclearinfo

--- Assets/Scripts/Main/Interface.cs
+++ Assets/Scripts/Main/Interface.cs
@@ -2,7 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SerializeField]
 public class Interface : MonoBehaviour
 {
+    public GameObject[] steps;
+    
+
+    private void Start()
+    {
+
+
+
+        UpdateProgress();
+
+
+    }
+    public void CheckClearStage(int stage_num)
+    {
+        if (GameDataManager.stageClearInfo[stage_num] != 0)
+        {
+
+
+            Transform childTransform = transform.Find("completed");
+
+            Debug.Log(childTransform);
+            if (childTransform != null)
+            {
+                GameObject completed = childTransform.gameObject;
+
+                completed.SetActive(true);
+            }
+
+
+        }
+    }
+    public void UpdateProgress() // update cleared stage 
+    {
+
   
+        for (int i = 0; i < steps.Length; i++)
+        {
+            if (GameDataManager.stageClearInfo[i+1] != 0)
+            {
+              
+              
+                Transform childTransform = steps[i].transform.Find("completed");
+
+                Debug.Log(childTransform);
+                if (childTransform != null)
+                {
+                    GameObject completed = childTransform.gameObject;
+
+                    completed.SetActive(true);
+                }
+             
+
+            }
+        }
+    }
 }
